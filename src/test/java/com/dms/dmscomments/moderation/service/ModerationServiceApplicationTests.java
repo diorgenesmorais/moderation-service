0d818486ea3ad921b42097cbba8134c527a066bf
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2025-05-09 20:35:42 -0300

    add projects spring

---
+++ src/test/java/com/dms/dmscomments/moderation/service/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.dmscomments.moderation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
